97ea005aaa782bf9c4058075c65170714f89e32b
Yujin Park <ypark55@jhu.edu>
2020-04-11 22:39:54 -0400

    Initial Commit

---
+++ Madlib Knockoff/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Madlib Knockoff
+//
+//  Created by 박유진 on 2020/04/11.
+//  Copyright © 2020 DEJ_Apps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
